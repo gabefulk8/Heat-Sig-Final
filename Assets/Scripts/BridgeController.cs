b07d10e834de5b339c482382a6365763fada9206
QuietH <hamzazagha@yahoo.com>
2023-02-20 14:35:28 -0600

    AI placement

--- Assets/Scripts/BridgeController.cs
+++ Assets/Scripts/BridgeController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class BridgeController : MonoBehaviour
 {
@@ -13,6 +14,8 @@ public class BridgeController : MonoBehaviour
 
     void Update()
     {
+       // AstarPath.active.Scan();
+
         if (Input.GetKeyDown(KeyCode.V))
         {
             bridgeAnimator.SetTrigger("TriggerBridge");
