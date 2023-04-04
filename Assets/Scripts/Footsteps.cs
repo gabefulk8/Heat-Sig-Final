4f5b5ed1c1b32e22ec225545fc8e53422872807f
gabefulk8 <89150841+gabefulk8@users.noreply.github.com>
2023-04-04 16:33:22 -0500

    Footsteps sounds in

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public AudioSource source;
+    public CharacterController playerController;
+    private AudioClip clip;
+    public AudioClip[] feetSnow;
+    public AudioClip[] feetMetal;
+    public AudioClip[] feetBoat;
+    public AudioClip[] feetWood;
+
+    private float footstepTimer = 0.42f;
+
+    void Update()
+    {
+        CallFootsteps();
+    }
+
+    void CallFootsteps()
+    {
+        if (playerController.velocity.magnitude > 0.2f)
+        {
+            footstepTimer -= Time.deltaTime;
+
+            if (footstepTimer <= 0)
+            {
+                if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit))
+                {
+                    switch (hit.collider.gameObject.tag)
+                    {
+                        case "Metal":
+                            clip = feetMetal[Random.Range(0, feetMetal.Length - 1)];
+                            source.PlayOneShot(clip);
+                            break;
+                        case "Boat":
+                            clip = feetBoat[Random.Range(0, feetBoat.Length - 1)];
+                            source.PlayOneShot(clip);
+                            break;
+                        case "Wood":
+                            clip = feetWood[Random.Range(0, feetWood.Length - 1)];
+                            source.PlayOneShot(clip);
+                            break;
+                        default:
+                            clip = feetSnow[Random.Range(0, feetSnow.Length)];
+                            source.PlayOneShot(clip);
+                            break;
+                    }
+                }
+
+                footstepTimer = 0.5f;
+            }
+        }
+    }
+}
