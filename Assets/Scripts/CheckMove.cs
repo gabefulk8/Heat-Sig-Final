fde8064b7058ed5fbe5ab5f5e07d2b7ddf8b82c5
QuietH <hamzazagha@yahoo.com>
2023-04-20 01:31:41 -0500

    AI Placement done working on scirptsW!!!!
    dasd awww w w hg hh

--- Assets/Scripts/CheckMove.cs
+++ Assets/Scripts/CheckMove.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CheckMove : MonoBehaviour
 {
     private Animator animator;
+    private AudioSource audioSource;
+
 
     const string WALK = "Walk"; // ChangeAnimationState(WALK);
     const string IDLE = "Idle"; // ChangeAnimationState(IDLE);
@@ -13,10 +15,15 @@ public class CheckMove : MonoBehaviour
 
     public Transform target;
 
+    public AudioClip[] footstepSounds; 
+    public float footstepInterval = 0.5f; 
+    private float footstepTimer;
 
     void Start()
     {
         animator = GetComponentInChildren<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        footstepTimer = 0f;
     }
 
 
@@ -31,6 +38,16 @@ public class CheckMove : MonoBehaviour
         else
         {
             animator.SetBool("IsMoving", false);
+        }
+
+        if (animator.GetBool("IsMoving"))
+        {
+            footstepTimer -= Time.deltaTime;
+            if (footstepTimer <= 0f)
+            {
+                footstepTimer = footstepInterval;
+                PlayFootstepSound();
+            }
         }
 
 
@@ -50,6 +67,15 @@ public class CheckMove : MonoBehaviour
         if (Vector3.Distance(transform.position, target.position) > 4)
         {
             animator.SetBool("IsAttack", false);
+        }
+    }
+
+    void PlayFootstepSound()
+    {
+        if (footstepSounds.Length > 0)
+        {
+            int index = Random.Range(0, footstepSounds.Length);
+            audioSource.PlayOneShot(footstepSounds[index], 10f);
         }
     }
 
